e2591553707779398fa3c195cbb4c4cbfc7a01fc
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2024-10-16 17:33:14 -0300

    asdas

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-let nome_usuario = prompt ("Qual é o seu nome?");
+let nome_usuario = "";
 let elemento = document.querySelector("#nome-usuario");
-elemento.textContent = nome_usuario;
+
+while(nome_usuario == ""){
+    nome_usuario = prompt("Qual é o seu  nome?");
+}
+
+if(nome_usuario == null){
+    elemento.textContent = "Seja muito bem vindo";
+}
+else{
+    elemento.textContent = nome_usuario;
+}
+
+
+
